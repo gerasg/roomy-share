617b2b3b23c5bda405436d79a1c54e78a85d310b
Gerard Gonzalez <gerard.gonzalez@t-mark.co.jp>
2023-07-06 12:21:47 +0900

    Dashboard styles wip

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,7 +1,9 @@
-//dashboard.tsx
+// dashboard.tsx
 import { useRouter } from 'next/router'
-import axios from 'axios'; // Asegúrate de instalar axios con npm install axios
+import axios from 'axios';
 import styles from '../styles/Dashboard.module.css'
+import { Navbar, Nav, Container, Button, Row, Col } from 'react-bootstrap';
+import { HouseFill, FileEarmark, Cart, People, GraphUp, Puzzle } from 'react-bootstrap-icons';
 
 export default function Dashboard() {
   const router = useRouter()
@@ -21,12 +23,35 @@ export default function Dashboard() {
     }
   }
 
-
   return (
-    <div className={`container py-5 ${styles['dashboard-container']}`}>
-      <h1 className="h3 mb-3 fw-normal text-center">Bienvenido a tu panel de control</h1>
-      <button className={`btn btn-lg btn-primary ${styles['logout-button']}`} onClick={handleLogout}>Cerrar sesión</button>
-      <button className={`btn btn-lg btn-secondary ${styles['assign-tasks-button']}`} onClick={assignTasks}>Asignar tareas ahora</button>
+    <div>
+      <Navbar className="bg-dark">
+        <Container fluid>
+          <Navbar.Brand href="#" className="text-white">Roomy Share | Dashboard</Navbar.Brand>
+          <Navbar.Toggle />
+          <Navbar.Collapse className="justify-content-end">
+            <Button className="btn btn-primary" onClick={assignTasks}>Asignar tareas ahora</Button>
+            <Button className="btn btn-danger ms-2" onClick={handleLogout}>Cerrar sesión</Button>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      <Container fluid>
+        <Row>
+          <Col xs={2} id="sidebar-wrapper">      
+            <Nav className="flex-column">
+              <Nav.Link href="#"><HouseFill /> Dashboard</Nav.Link>
+              <Nav.Link href="#"><FileEarmark /> Orders</Nav.Link>
+              <Nav.Link href="#"><Cart /> Products</Nav.Link>
+              <Nav.Link href="#"><People /> Customers</Nav.Link>
+              <Nav.Link href="#"><GraphUp /> Reports</Nav.Link>
+              <Nav.Link href="#"><Puzzle /> Integrations</Nav.Link>
+            </Nav>
+          </Col>
+          <Col xs={10} id="page-content-wrapper">
+            <h1>MAMAHUEVO</h1>
+          </Col>
+        </Row>
+      </Container>
     </div>
   )
 }
