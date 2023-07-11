e66e6d4ccc181e88d4b61e7910912a00aca75c08
Gerard Gonzalez <gerard.gonzalez@t-mark.co.jp>
2023-07-12 01:43:41 +0900

    Dashboard styling WIP

--- src/pages/user_dashboard.tsx
+++ src/pages/user_dashboard.tsx
@@ -1,24 +1,140 @@
 // user_dashboard.tsx
-import { useRouter } from 'next/router'
-import axios from 'axios';
+import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
-import { Navbar, Nav, Container, Button, Row, Col, Table, Badge } from 'react-bootstrap';
-import { HouseFill, FileEarmark, Cart, People, GraphUp, Puzzle } from 'react-bootstrap-icons';
+import { Badge } from 'react-bootstrap';
+import { HouseFill } from 'react-bootstrap-icons';
+import styled from 'styled-components';
+
+const DashboardWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  min-height: 100vh;
+  background: #18191F;
+  color: #FFFFFF;
+
+  @media (max-width: 768px) {
+    flex-direction: column;
+  }
+`;
+
+const TableContainer = styled.div`
+  overflow-x: auto;
+`;
+
+const SideNav = styled.div`
+  flex: 0 0 240px;
+  background: #212431;
+  padding: 2rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  @media (max-width: 768px) {
+    flex-direction: row;
+    justify-content: space-between;
+    flex: 0 0 auto;
+  }
+`;
+
+const MainContent = styled.div`
+  flex-grow: 1;
+  padding: 2rem;
+
+  @media (max-width: 768px) {
+    padding: 1rem;
+  }
+`;
+
+const NavItem = styled.a`
+  display: flex;
+  align-items: center;
+  padding: 0.5rem 0;
+  color: #FFFFFF;
+  text-decoration: none;
+  margin-bottom: 1.5rem;
+  font-size: 1.5rem;
+  gap: 1rem;
+  transition: 0.3s ease-in-out;
+
+  &:hover {
+    color: #1ABC9C;
+    cursor: pointer;
+  }
+
+  @media (max-width: 768px) {
+    padding: 0.5rem;
+    margin-bottom: 0;
+  }
+`;
+
+const Card = styled.div`
+  background: #2C2F47;
+  border-radius: 1rem;
+  padding: 1.5rem;
+  margin-bottom: 2rem;
+  color: #FFFFFF;
+
+  @media (max-width: 768px) {
+    overflow-x: auto;
+  }
+`;
+
+const Table = styled.table`
+  width: 100%;
+  border-collapse: collapse;
+  text-align: left;
+`;
+
+const TableRow = styled.tr`
+  border-bottom: 1px solid #353848;
+
+  &:last-child {
+    border-bottom: none;
+  }
+`;
+
+const TableData = styled.td`
+  padding: 1rem 0;
+  color: #FFFFFF;
+
+  &:first-child {
+    padding-right: 2rem;
+  }
+`;
+
+const Button = styled.button`
+  background: #1ABC9C;
+  border: none;
+  padding: 0.75rem 1.5rem;
+  color: #FFFFFF;
+  border-radius: 0.5rem;
+  cursor: pointer;
+  transition: 0.3s ease-in-out;
+
+  &:hover {
+    background: #16A085;
+  }
+`;
+
+const StyledCheckbox = styled.input`
+  width: 20px;
+  height: 20px;
+  cursor: pointer;
+`;
 
 export default function Dashboard() {
-  const router = useRouter()
+  const router = useRouter();
   const [tasks, setTasks] = useState([]);
 
   useEffect(() => {
-    // Verificar que estamos en el cliente antes de hacer la llamada al servidor
     if (typeof window !== 'undefined') {
       fetchTasks();
     }
   }, []);
 
   const handleLogout = () => {
-    localStorage.removeItem('token')
-    router.push('/login')
+    localStorage.removeItem('token');
+    router.push('/login');
   }
 
   const fetchTasks = async () => {
@@ -43,73 +159,7 @@ export default function Dashboard() {
     }
   };
 
-  return (
-    <div>
-      <Navbar className="bg-dark">
-        <Container fluid>
-          <Navbar.Brand href="#" className="text-white">Roomy Share | Dashboard</Navbar.Brand>
-          <Navbar.Toggle />
-          <Navbar.Collapse className="justify-content-end">
-            <Button className="btn btn-danger" onClick={handleLogout}>Cerrar sesión</Button>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-      <Container fluid>
-        <Row>
-          <Col xs={2} id="sidebar-wrapper">      
-            <Nav className="flex-column">
-              <Nav.Link href="#"><HouseFill /> Dashboard</Nav.Link>
-              <Nav.Link href="#"><FileEarmark /> Orders</Nav.Link>
-              <Nav.Link href="#"><Cart /> Products</Nav.Link>
-              <Nav.Link href="#"><People /> Customers</Nav.Link>
-              <Nav.Link href="#"><GraphUp /> Reports</Nav.Link>
-              <Nav.Link href="#"><Puzzle /> Integrations</Nav.Link>
-            </Nav>
-          </Col>
-          <Col xs={10} id="page-content-wrapper">
-            <h1 className="h3 mb-3 fw-normal text-center">Bienvenido a tu panel de control</h1>
-            <h1 className="h3 mb-3 fw-normal text-center">TENANT</h1>
-            <div className="tasks">
-              <Table striped bordered hover responsive>
-                <thead>
-                  <tr>
-                    <th>Fecha</th>
-                    <th>Task</th>
-                    <th>Responsable</th>
-                    <th>Status</th>
-                    <th></th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {tasks.map((task, i) => {
-                    const taskDate = new Date(task.day);
-                    const now = new Date();
-                    const isTaskDateTodayOrInFuture = taskDate >= now;
-
-                    return (
-                      <tr key={i}>
-                        <td>{taskDate.toLocaleDateString()}</td>
-                        <td>{task.task}</td>
-                        <td>{task.name} {task.lastname}</td>
-                        <td>{task.completed ? <Badge bg="success">Completado</Badge> : <Badge bg="danger">Pendiente</Badge>}</td>
-                        <td>
-                          {!task.completed && isTaskDateTodayOrInFuture && (
-                            <Button onClick={() => handleMarkAsComplete(task)}>Marcar como completo</Button>
-                          )}
-                        </td>
-                      </tr>
-                    );
-                  })}
-                </tbody>
-              </Table>
-            </div>
-          </Col>
-        </Row>
-      </Container>
-    </div>
-  )
-
-  async function handleMarkAsComplete(task) {
+  const handleMarkAsComplete = async (task) => {
     const token = localStorage.getItem('token');
     if (!token) {
       return;
@@ -130,4 +180,57 @@ export default function Dashboard() {
       console.error('Error marking task as completed:', response.statusText);
     }
   }
+
+  return (
+    <DashboardWrapper>
+      <SideNav>
+        <NavItem href="/"><HouseFill /></NavItem>
+      </SideNav>
+      <MainContent>
+        <Card>
+          <h1>Bienvenido a tu panel de control</h1>
+          <Button onClick={handleLogout}>Cerrar sesión</Button>
+        </Card>
+        <Card>
+        <TableContainer>
+          <Table>
+              <thead>
+                <TableRow>
+                  <TableData>Fecha</TableData>
+                  <TableData>Task</TableData>
+                  <TableData>Status</TableData>
+                  <TableData></TableData>
+                </TableRow>
+              </thead>
+              <tbody>
+              {tasks.map((task, i) => {
+                const taskDate = new Date(task.day);
+                taskDate.setHours(0,0,0,0);
+                const now = new Date();
+                now.setHours(0,0,0,0);
+          
+                return (
+                  <TableRow key={i}>
+                    <TableData>{taskDate.toLocaleDateString()}</TableData>
+                    <TableData>{task.task}</TableData>
+                    <TableData>{task.completed ? <Badge bg="success">Completado</Badge> : <Badge bg="danger">Pendiente</Badge>}</TableData>
+                    <TableData>
+                      <StyledCheckbox 
+                        type="checkbox" 
+                        checked={task.completed} 
+                        onChange={() => handleMarkAsComplete(task)} 
+                        disabled={task.completed} 
+                      />
+                    </TableData>
+                  </TableRow>
+                );
+              })}
+            </tbody>
+          </Table>
+        </TableContainer>
+
+        </Card>
+      </MainContent>
+    </DashboardWrapper>
+  );
 }
