8654d13a8688d604199691f07db7f60b9354eb4b
Gerard Gonzalez <gerard.gonzalez@t-mark.co.jp>
2023-07-17 04:58:35 +0900

    React table implementation

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,9 +1,10 @@
 import { useRouter } from 'next/router';
 import axios from 'axios';
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useMemo } from 'react';
 import { Badge } from 'react-bootstrap';
 import { HouseFill } from 'react-bootstrap-icons';
 import styled from 'styled-components';
+import { useTable, useSortBy } from 'react-table';
 
 const DashboardWrapper = styled.div`
   display: flex;
@@ -194,7 +195,63 @@ export default function Dashboard() {
     } else {
       console.error('Error fetching payments:', response.statusText);
     }
-  };  
+  }; 
+  
+  const columnsTasks = useMemo(
+    () => [
+      {
+        Header: "Fecha",
+        accessor: "day",
+        Cell: ({ value }) => new Date(value).toLocaleDateString()
+      },
+      {
+        Header: "Task",
+        accessor: "task"
+      },
+      {
+        Header: "Status",
+        accessor: "completed",
+        Cell: ({ value }) => value ? <Badge bg="success">Completado</Badge> : <Badge bg="danger">Pendiente</Badge>
+      },
+    ],
+    []
+  );
+
+  const columnsPayments = useMemo(
+    () => [
+      {
+        Header: "Fecha de pago",
+        accessor: "payment_date",
+        Cell: ({ value }) => new Date(value).toLocaleDateString()
+      },
+      {
+        Header: "Cantidad pagada",
+        accessor: "amount_paid"
+      },
+      {
+        Header: "Estado",
+        accessor: "status",
+        Cell: ({ value }) => value === 'paid' ? <Badge bg="success">Pagado</Badge> : <Badge bg="danger">Pendiente</Badge>
+      },
+    ],
+    []
+  );
+
+  const {
+    getTableProps: getTablePropsTasks,
+    getTableBodyProps: getTableBodyPropsTasks,
+    headerGroups: headerGroupsTasks,
+    rows: rowsTasks,
+    prepareRow: prepareRowTasks,
+  } = useTable({ columns: columnsTasks, data: tasks }, useSortBy);
+
+  const {
+    getTableProps: getTablePropsPayments,
+    getTableBodyProps: getTableBodyPropsPayments,
+    headerGroups: headerGroupsPayments,
+    rows: rowsPayments,
+    prepareRow: prepareRowPayments,
+  } = useTable({ columns: columnsPayments, data: payments }, useSortBy);
 
   return (
     <DashboardWrapper>
@@ -208,53 +265,59 @@ export default function Dashboard() {
         </Card>
         <Card>
         <TableContainer>
-          <Table>
-              <thead>
-                <TableRow>
-                  <TableData>Fecha</TableData>
-                  <TableData>Task</TableData>
-                  <TableData>Status</TableData>
+          <Table {...getTablePropsTasks()}>
+            <thead>
+              {headerGroupsTasks.map((headerGroup) => (
+                <TableRow {...headerGroup.getHeaderGroupProps()}>
+                  {headerGroup.headers.map((column) => (
+                    <TableData {...column.getHeaderProps(column.getSortByToggleProps())}>
+                      {column.render('Header')}
+                      {column.isSorted ? (column.isSortedDesc ? ' 🔽' : ' 🔼') : ''}
+                    </TableData>
+                  ))}
                 </TableRow>
-              </thead>
-              <tbody>
-              {tasks.map((task, i) => (
-                  <TableRow key={i}>
-                    <TableData>{new Date(task.day).toLocaleDateString()}</TableData>
-                    <TableData>{task.task}</TableData>
-                    <TableData>{task.completed ? <Badge bg="success">Completado</Badge> : <Badge bg="danger">Pendiente</Badge>}</TableData>
+              ))}
+            </thead>
+            <tbody {...getTableBodyPropsTasks()}>
+              {rowsTasks.map((row) => {
+                prepareRowTasks(row);
+                return (
+                  <TableRow {...row.getRowProps()}>
+                    {row.cells.map((cell) => <TableData {...cell.getCellProps()}>{cell.render('Cell')}</TableData>)}
                   </TableRow>
-                ))}
+                );
+              })}
             </tbody>
           </Table>
-        </TableContainer>
-
+      </TableContainer>
         </Card>
         <Card>
-          <TableContainer>
-            <Table>
-              <thead>
-                <TableRow>
-                  <TableData>Fecha de pago</TableData>
-                  <TableData>Cantidad pagada</TableData>
-                  <TableData>Estado</TableData>
-                </TableRow>
-              </thead>
-              <tbody>
-                {payments.map((payment, i) => (
-                  <TableRow key={i}>
-                    <TableData>{new Date(payment.payment_date).toLocaleDateString()}</TableData>
-                    <TableData>{payment.amount_paid}</TableData>
-                    <TableData>
-                      {payment.status === 'paid' 
-                        ? <Badge bg="success">Pagado</Badge> 
-                        : <Badge bg="danger">Pendiente</Badge>
-                      }
-                    </TableData>
-                  </TableRow>
+        <TableContainer>
+        <Table {...getTablePropsPayments()}>
+          <thead>
+            {headerGroupsPayments.map((headerGroup) => (
+              <TableRow {...headerGroup.getHeaderGroupProps()}>
+                {headerGroup.headers.map((column) => (
+                  <TableData {...column.getHeaderProps(column.getSortByToggleProps())}>
+                    {column.render('Header')}
+                    {column.isSorted ? (column.isSortedDesc ? ' 🔽' : ' 🔼') : ''}
+                  </TableData>
                 ))}
-              </tbody>
-            </Table>
-          </TableContainer>
+              </TableRow>
+            ))}
+          </thead>
+          <tbody {...getTableBodyPropsPayments()}>
+            {rowsPayments.map((row) => {
+              prepareRowPayments(row);
+              return (
+                <TableRow {...row.getRowProps()}>
+                  {row.cells.map((cell) => <TableData {...cell.getCellProps()}>{cell.render('Cell')}</TableData>)}
+                </TableRow>
+              );
+            })}
+          </tbody>
+        </Table>
+      </TableContainer>
         </Card>
       </MainContent>
     </DashboardWrapper>
